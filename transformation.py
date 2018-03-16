c1c096f6d2ea5d6c39cac0bc5f6e2029d95fc99f
Julien Marin <marin.julien@etu.umontpellier.fr>
2018-03-16 17:39:15 +0100

    rédaction de doc

--- transformation.py
+++ transformation.py
@@ -9,10 +9,6 @@ class TaggedWord():
     def __str__(self):
         return self.word+"[" + self.pos + "]"
 
-# class RuleFactory():
-#     def __init__(str)
-
-
 class Rule():
     ''' Règle de transformation de mot vers un autre '''
     def __init__(self, input_pos, output_pos, input_regex, output_regex):
@@ -20,35 +16,22 @@ class Rule():
         self.output_pos = output_pos
         self.input_regex = input_regex
         self.output_regex = output_regex
-        
-    def _parse(self, string):
-        tokens = string.split(";")
-        
-        
-    def isAppliable(self, mot):
+                
+    def isAppliable(self, taggedword):
         '''Renvoie vrai si la règle est applicable sur le mot renseigné. Il
         s'agit de vérifier que le mot aie (1) la classe grammaticale
         attendue et (2) que la forme du mot match avec le regex
         attendu
         '''
-        return (mot.pos == self.input_pos
-                and (re.match(self.input_regex, mot.word) is not None))
+        return (taggedword.pos == self.input_pos
+                and (re.match(self.input_regex, taggedword.word) is not None))
 
     def production(self, taggedword):
-        import re
-        # newstring = re.sub('(Banana)', r'\1Toothpaste', oldstring)
+        ''' Application de la règle sur le mot renseigné. L'usage de cette fonction suppose que la règle est applicable (isAplliable) '''
         return re.sub(self.input_regex, self.output_regex, taggedword.word)
-
-    # def productions(self, taggedwords):
-    #     res = []
-    #     for m in taggedwords:
-    #         if self.isAppliable(m):
-    #             res += [self.production(m.word)]
-    #     return res
 
 def main():
     mot_exemple = TaggedWord("manger", "VERBE_1er_groupe")
-                    # TaggedWord("braquer", "VERBE_1er_groupe")
     regle_exemple = Rule("VERBE_1er_groupe", "NOM", "(.*)er", "\\1eable")
     print("La règle est elle applicable sur", mot_exemple, "?")
     print(regle_exemple.isAppliable(mot_exemple))
