a5ea7a72b29461613e453ddad776cae89564cce0
Julien Marin <marin.julien@etu.umontpellier.fr>
2018-04-16 14:23:56 +0200

    résolution import pb relatifs

---
+++ jdm_api/config.py
@@ -0,0 +1,13 @@
+import logging
+
+import os
+import sys
+sys.path.append(os.getcwd())
+
+logging.basicConfig(format="%(levelname)-7s %(name)-15s%(lineno)s:%(message)s", level=logging.DEBUG)
+DUMP_PATH = "./dumps/"
+DUMP_EXTRACTION_REGEX_PATH = "./regex_dump_extraction.txt"
+
+class Loggable():
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
